7f324a29000420b229bd09b49aae6ef377c02e9a
nilssck <94846325+nilssck@users.noreply.github.com>
2024-02-20 17:53:24 +0100

    Redo Tileset with Terrain, Redo Levels (#44)
    * New Desert TileSet with Terrain
    
    * Add Grass Tileset and Level
    
    * Fix Level 3

--- Code/Scripts/Movement/Player.cs
+++ Code/Scripts/Movement/Player.cs
@@ -7,59 +7,59 @@ but that would conflict with the Player class name. */
 namespace Movement;
 public partial class Player : CharacterBody2D
 {
-    [Export] public float speed { get; set; }
-    [Export] public float RotationSpeed { get; set; } = 1.5f;
-    public GunController Gun { get; set; }
-    public AnimationHandler AnimationHandler { get; set; }
-    private float _rotationDirection;
-    protected TrailComponent trailComponent;
-    
-    public override void _Ready()
-    {
-        Gun = GetNode<GunController>("Gun");
-        AnimationHandler = GetNode<AnimationHandler>("AnimationPlayer");
-        trailComponent = GetNode<TrailComponent>("Trail");
-    }
+	[Export] public float speed { get; set; }
+	[Export] public float RotationSpeed { get; set; } = 1.5f;
+	public GunController Gun { get; set; }
+	public AnimationHandler AnimationHandler { get; set; }
+	private float _rotationDirection;
+	protected TrailComponent trailComponent;
+	
+	public override void _Ready()
+	{
+		Gun = GetNode<GunController>("Gun");
+		AnimationHandler = GetNode<AnimationHandler>("AnimationPlayer");
+		trailComponent = GetNode<TrailComponent>("Trail");
+	}
 
-    public override void _PhysicsProcess(double delta)
-    {
-        //Movement
-        GetInput();
-        Rotation += _rotationDirection * RotationSpeed * (float)delta;
-        MoveAndSlide();
+	public override void _PhysicsProcess(double delta)
+	{
+		//Movement
+		GetInput();
+		Rotation += _rotationDirection * RotationSpeed * (float)delta;
+		MoveAndSlide();
 
-        //Emit trail
-        if (IsMoving())
-        {
-            trailComponent.EmitTrail();
-        }
-       
-        //Gun controlling
-        Gun.RotateTowards(GetGlobalMousePosition());
-        if (Input.IsActionPressed("left_mouse"))
-        {
-            Gun.Shoot();
-        }
-    }
+		//Emit trail
+		if (IsMoving())
+		{
+			trailComponent.EmitTrail();
+		}
+	   
+		//Gun controlling
+		Gun.RotateTowards(GetGlobalMousePosition());
+		if (Input.IsActionPressed("left_mouse"))
+		{
+			Gun.Shoot();
+		}
+	}
 
-    public void GetInput()
-    {
-        Vector2 input = Input.GetVector("left", "right", "down", "up");
+	public void GetInput()
+	{
+		Vector2 input = Input.GetVector("left", "right", "down", "up");
 
-        AnimationHandler.PlayAnimationOfInput(input);
+		AnimationHandler.PlayAnimationOfInput(input);
 
-        _rotationDirection = input.X;
-        Velocity = calculateVelocity(Transform.X * input.Y);
-    }
+		_rotationDirection = input.X;
+		Velocity = calculateVelocity(Transform.X * input.Y);
+	}
 
-    public Vector2 calculateVelocity(Vector2 move_input)
-    {
-        Vector2 velocity = move_input * speed; //set the velocity to the input times the speed.
-        return velocity;
-    }
+	public Vector2 calculateVelocity(Vector2 move_input)
+	{
+		Vector2 velocity = move_input * speed; //set the velocity to the input times the speed.
+		return velocity;
+	}
 
-    public bool IsMoving()
-    {
-        return Velocity != Vector2.Zero;
-    }
+	public bool IsMoving()
+	{
+		return Velocity != Vector2.Zero;
+	}
 }
