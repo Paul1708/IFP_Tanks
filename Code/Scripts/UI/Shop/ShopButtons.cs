b76a493bcfbe21d18d885433e87a81ef2573b28e
Paul1708 <brinkmann-lich@web.de>
2024-02-28 15:00:10 +0100

    -1 if

--- Code/Scripts/UI/Shop/ShopButtons.cs
+++ Code/Scripts/UI/Shop/ShopButtons.cs
@@ -30,24 +30,14 @@ public partial class ShopButtons : Button
 		}
 	}
 
-	public void SetWeaponButtonStates()
-	{
-		var greatGreatGrandParent = this.GetParent().GetParent().GetParent().GetParent();
+public void SetWeaponButtonStates()
+{
+    var greatGreatGrandParent = this.GetParent().GetParent().GetParent().GetParent();
 
-		if (greatGreatGrandParent.IsInGroup("Weapons"))
-		{
-			if (ShopManager.Instance.panelIndexMap.TryGetValue(GetParent().Name, out int index))
-			{
-				var weapon = ShopManager.Instance.weaponsList[index];
-				if (weapon.unlocked)
-				{
-					this.Text = weapon.equipped ? "Equipped" : "Equip";
-				}
-				else
-				{
-					this.Text = "Buy";
-				}
-			}
-		}
-	}
+    if (greatGreatGrandParent.IsInGroup("Weapons") && ShopManager.Instance.panelIndexMap.TryGetValue(GetParent().Name, out int index))
+    {
+        var weapon = ShopManager.Instance.weaponsList[index];
+        this.Text = weapon.unlocked ? (weapon.equipped ? "Equipped" : "Equip") : "Buy";
+    }
+}
 }
