38d03964bd2d9d0aff9e489d10277d6c7a659da9
BuildTools <gero.brunke@stud.uni-heidelberg.de>
2024-03-19 19:17:40 +0100

    Add Behaviours Templates

--- Code/Scripts/Enemies/BasicBehaviour.cs
+++ Code/Scripts/Enemies/BasicBehaviour.cs
@@ -2,14 +2,14 @@ namespace Enemies;
 
 public partial class BasicBehaviour : Behaviour
 {
-    public override void Setup()
-    {
-        Navigation.NavigateTowards(Player.GlobalPosition);
-    }
-    public override void ExecuteBehaivour()
-    {
-        Gun.RotateTowards(Player.GlobalPosition);
-        Gun.Shoot();
-        Navigation.NavigateTowards(Player.GlobalPosition);
-    }
+	public override void Setup()
+	{
+		Navigation.NavigateTowards(Player.GlobalPosition);
+	}
+	public override void ExecuteBehaivour()
+	{
+		Gun.RotateTowards(Player.GlobalPosition);
+		Gun.Shoot();
+		Navigation.NavigateTowards(Player.GlobalPosition);
+	}
 }
---
+++ Code/Scripts/Enemies/KamikazeBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using Godot;
+
+namespace Enemies;
+
+public partial class KamikazeBehaviour : Behaviour
+{
+    
+    
+    //TODO: Bullets from enemies cannot hit other enemies
+    //TODO: Laser only damages in Bullet-Hit rate not every frame
+    //TODO: Merge Develop with current branch for updated Bullet stats
+    //TODO: When two bullets of same group i.e. shot by enemy collide, destroy the weaker one, if they are the same type delete a random one
+    
+    public override void Setup()
+    {
+        Navigation.NavigateTowards(Player.GlobalPosition);
+        //Navigation.PathDesiredDistance = 9;
+        //Navigation.PathMaxDistance = 9;
+    }
+
+    public override void ExecuteBehaivour()
+    {
+        if (Navigation.IsTargetReached())
+        {
+            _explode();
+            return;
+        }
+        
+        Navigation.NavigateTowards(Player.GlobalPosition);
+    }
+
+    private void _explode()
+    {
+        
+    }
+}
