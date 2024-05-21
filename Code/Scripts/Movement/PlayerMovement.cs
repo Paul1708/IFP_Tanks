a329b13afe158cdf4b82a3e7f527f5233a387adf
Paul1708 <145999195+Paul1708@users.noreply.github.com>
2024-05-21 20:23:13 +0200

    added death screen and victory screen (#67)
    * added deathscreen prototype
    
    * basic deathscreen finished
    
    * removed not used methods
    
    * add spaces to buttons to fit
    
    * added basic victory screen
    
    * added special effects to vicroryScreen
    
    * make fireworkcooldown export

--- Code/Scripts/Movement/PlayerMovement.cs
+++ Code/Scripts/Movement/PlayerMovement.cs
@@ -10,6 +10,7 @@ public partial class PlayerMovement : CharacterBody2D
 	public GunController Gun { get; set; }
 	public AnimationHandler AnimationHandler { get; set; }
 	private float _rotationDirection;
+	private GpuParticles2D _deathParticles;
 	protected TrailComponent trailComponent;
 
 	public override void _Ready()
@@ -17,6 +18,8 @@ public partial class PlayerMovement : CharacterBody2D
 		Gun = GetNode<GunController>("Gun");
 		AnimationHandler = GetNode<AnimationHandler>("AnimationPlayer");
 		trailComponent = GetNode<TrailComponent>("TrailComponent");
+		_deathParticles = GetNode<GpuParticles2D>("DeathParticles");
+		PlayerManager.Instance.PlayerHealthComponent.OnDeath += OnDeath;
 	}
 
 	public override void _PhysicsProcess(double delta)
@@ -40,6 +43,11 @@ public partial class PlayerMovement : CharacterBody2D
 		}
 	}
 
+	public override void _ExitTree()
+	{
+		PlayerManager.Instance.PlayerHealthComponent.OnDeath -= OnDeath;
+	}
+
 	public void GetInput()
 	{
 		Vector2 input = Input.GetVector("left", "right", "down", "up");
@@ -59,5 +67,12 @@ public partial class PlayerMovement : CharacterBody2D
 	public bool IsMoving()
 	{
 		return Velocity != Vector2.Zero;
+	}
+
+	private async void OnDeath()
+	{
+		_deathParticles.Emitting = true;
+		await ToSignal(_deathParticles, "finished");
+		QueueFree();
 	}
 }
---
+++ Code/Scripts/UI/VictoryScreen.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Threading.Tasks;
+using Godot;
+using Managers.Level;
+
+public partial class VictoryScreen : Control
+{
+	private LevelManager _levelManager;
+	private AnimationPlayer _blurAnimation;
+	private ColorRect _blur;
+	private MusicController _musicController;
+	private ParticleController _particleController;
+
+	[Export]
+	public float FireworkCooldown = 0.2f;
+
+	[Signal]
+	public delegate void OnBacktoMainMenuPressedEventHandler();
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		_blur = GetNode<ColorRect>("Blur");
+		_blurAnimation = GetNode<AnimationPlayer>("BlurAnimation");
+		_levelManager = GetTree().GetFirstNodeInGroup("LevelManager") as LevelManager;
+		_musicController = GetNode<MusicController>("/root/MusicController");
+		_particleController = GetNode<ParticleController>("/root/ParticleController");
+
+
+		_levelManager.OnGameWonShowVictoryScreen += ShowVictoryScreen;
+
+		Hide();
+	}
+
+	public override void _ExitTree()
+	{
+		_levelManager.OnGameWonShowVictoryScreen -= ShowVictoryScreen;
+	}
+
+	public async void ShowVictoryScreen()
+	{
+		_blurAnimation.Play("LevelCooldown");
+		GetTree().Paused = true;
+		Show();
+
+		while (IsVisibleInTree())
+		{
+			GenerateFireworks();
+			await ToSignal(GetTree().CreateTimer(FireworkCooldown), "timeout");
+		}
+	}
+
+	private void OnReturnPressed()
+	{
+		GetTree().Paused = false;
+		_musicController.Play(Sound.ButtonClick);
+		EmitSignal(SignalName.OnBacktoMainMenuPressed);
+		Hide();
+	}
+
+	private void GenerateFireworks()
+	{
+		// Get the size of the window
+		Vector2 windowSize = GetViewportRect().Size;
+
+		// Generate a random position within the window
+		Random random = new();
+		Vector2 randomPosition = new(random.Next((int)windowSize.X), random.Next((int)windowSize.Y));
+
+		// Emit particles at the random position
+		_particleController.EmitParticles(randomPosition, Scene.Fireworks);
+	}
+}
